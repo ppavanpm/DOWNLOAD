37c09d890d506cbfef2642fab35883bd25154203
ppavanpm <indiaspsbsb@gmail.com>
2025-05-30 12:35:29 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,19 +1,21 @@
-from pytube import YouTube
+import yt_dlp
 import os
 import re
 
 def sanitize_filename(name):
-    # Remove invalid filename chars
     return re.sub(r'[\\/*?:"<>|]', "", name)
 
 def download_youtube_video(url):
-    yt = YouTube(url)
-    stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    if not stream:
-        raise Exception("No suitable video streams found")
-
-    title = sanitize_filename(yt.title)
-    filename = "temp_video.mp4"
-    stream.download(filename=filename)
-
+    ydl_opts = {
+        'format': 'mp4',
+        'outtmpl': 'temp_video.%(ext)s',
+        'quiet': True,
+        'no_warnings': True,
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        title = sanitize_filename(info_dict.get('title', 'video'))
+        ext = info_dict.get('ext', 'mp4')
+        filename = f"temp_video.{ext}"
     return filename, title
